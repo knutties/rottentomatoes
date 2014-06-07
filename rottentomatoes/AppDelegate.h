f9b55f2f063742a5877a7a78d5ca277d2ff666f2
natarajk <natarajk@yahoo-inc.com>
2014-06-07 15:56:01 -0700

    Initial Commit

---
+++ rottentomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottentomatoes
+//
+//  Created by Natarajan Kannan on 6/7/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\natarajk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rottentomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rottentomatoes
+//
+//  Created by Natarajan Kannan on 6/7/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\natarajk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
